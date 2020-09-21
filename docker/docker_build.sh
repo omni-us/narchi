d5daee4b177c168bd756c52fe435dc70474729f8
Mauricio Villegas <mauricio_ville@yahoo.com>
2020-09-21 09:58:11 +0200

    - Updated base docker images in circleci config. - Added generation of requirements.txt in docker build script.

--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -2,6 +2,12 @@
 
 V=$(date -u +%Y.%m.%d);
 
+./wheel_info.py requirements \
+    ../dist/narchi-*-py3-none-any.whl \
+    --extras_require pytorch \
+  | grep -v torch \
+  > requirements.txt;
+
 docker build -t mauvilsa/narchi-test:$V-py38 .;
 
 for py in 3.6 3.7; do
